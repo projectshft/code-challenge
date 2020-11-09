b428f2fdda882f3d575b45202e1eb0ff23811b11
Aaron Hayslip <amhayslip@gmail.com>
2020-11-08 20:03:43 -0500

    Create submission-template.js

---
+++ submission-template.js
@@ -0,0 +1,28 @@
+const findSum = function(array) {
+  // your code here - don't forget to return a number!
+};
+
+const findFrequency = function(array) {
+  // your code here - don't forget to return an object!
+};
+
+const isPalindrome = function(str) {
+  // your code here - don't forget to return a boolean!
+};
+
+const largestPair = function(array) {
+  // your code here - don't forget to return a number!
+};
+
+const removeParenth = function(str) {
+  // your code here - don't forget to return a string!
+};
+
+
+const scoreScrabble = function(str) {
+  // your code here - don't forget to return a number!
+};
+
+
+
+
